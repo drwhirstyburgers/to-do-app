856848ab151ca0f47f66dca22d138b60c050ed98
David Hirst <34040303+drwhirstyburgers@users.noreply.github.com>
2018-01-15 13:53:20 -0500

    final version

--- app.js
+++ app.js
@@ -1,37 +1,66 @@
 window.onload = function() {
   alert("The window has loaded!");
   onReady();
-}
+};
 
 function onReady() {
-  // a series of variables assigned to corresponding HTML elements
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById(toDoList);
+  const toDoList = document.getElementById('toDoList');
 
-  //need an event listener for when the user clicks to submit the addToDoForm
+  //add event listener for the addToDoForm
   addToDoForm.addEventListener('submit', () => {
     event.preventDefault();
-  //getting the text
+
+  //Next we need to code for what we want to happen when the code is submitted
+
+  //get the text from newToDoText
   let title = newToDoText.value;
 
-  //create a new li
+  //create a new li in the ul block
   let newLi = document.createElement('li');
 
-  //create a new input
+  // create a new input
   let checkbox = document.createElement('input');
+
+  //creat another input for a remove button
+  let removeButton = document.createElement('input');
 
   //set the input's type to checkbox
   checkbox.type = "checkbox";
 
-  //set the title
+  //set the second input type to button
+  removeButton.type = "button";
+
+  // REMOVE to do
+  removeButton.onclick = function(e){ deleteTodo(e)};
+
+  //set the text in the button to 'remove'
+  removeButton.value = "delete";
+
+  //etsablish the type of the new button
+  removeButton.type = "submit";
+
+  //set the text content of the new li to the let title using 'textContent'
   newLi.textContent = title;
 
-  //attach the checkbox to the Li
-  newLi. appendChild(checkbox);
+  //attache the checkbox to the new Li
+  newLi.appendChild(checkbox);
 
-  //attach the Li to the UL
+  //append the button to the Li
+  newLi.appendChild(removeButton);
+
+  //attache the new li to the ul
   toDoList.appendChild(newLi);
 
-  });
-}
+  //empty the input
+  newToDoText.value = '';
+
+  function deleteTodo(e){
+  console.log(e)
+        let li = e.target.parentNode;
+                 let ul = li.parentNode;
+                 ul.removeChild(li);
+               }
+    });
+  }
