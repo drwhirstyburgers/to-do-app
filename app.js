d6caf35e4359b86b1cd8286ffcf3148cc99a7147
David Hirst <34040303+drwhirstyburgers@users.noreply.github.com>
2018-01-15 19:10:18 -0500

    working version with delete button

--- app.js
+++ app.js
@@ -3,18 +3,28 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
+  let id = 0;
 
   function createNewToDo() {
   if (!newToDoText.value) { return; }
   toDos.push({
   title: newToDoText.value,
   complete: false,
+  id:
   });
 
+  id++;
 
   newToDoText.value = '';
 
   renderTheUI();
+  }
+
+  function deleteToDo(item.id) {
+    toDos = toDos.filter(item => item.id !== id);
+    if(toDos == true){
+      remove(toDo);
+    }
   }
 
   function renderTheUI() {
@@ -26,6 +36,12 @@ function onReady() {
          const newLi = document.createElement('li');
          const checkbox = document.createElement('input');
          checkbox.type = "checkbox";
+         const deleteButton = document.createElement('input');
+         deleteButton.type = "button";
+         deleteButton.addEventListener('click', function() { deleteToDo(toDo.id);
+         renderTheUI();
+       });
+
 
          newLi.textContent = toDo.title;
 
